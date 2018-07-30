b7ace2974c9a711aedadc938cee2934dd5bd71b6
dielduarte <magdiel.kesller@gmail.com>
2018-07-30 23:02:57 +0200

    saved

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -3,21 +3,22 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'that work giraffe brush giraffe intact firm cause member move icon harbor',
+  'panther rose must aspect shell aunt miss avoid hobby plate reduce conduct',
   'https://rinkeby.infura.io/qZtLwXWnvCYPAsOoIQ6E'
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
- const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
- console.log('attempting to deploy from account', accounts[0]);
+  console.log('attempting to deploy from account', accounts[0]);
 
- const result = await new web3.eth.Contract(JSON.parse(interface))
-  .deploy({ data:  bytecode })
-  .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data:  bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 
